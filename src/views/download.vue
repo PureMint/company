0b06674346d2430e742d349d2fa37a429a62476e
huiliaoliao <31496673+huiliaoliao@users.noreply.github.com>
2017-10-23 13:26:50 +0800

    update
    11

--- src/views/download.vue
+++ src/views/download.vue
@@ -1,55 +1,177 @@
 <template>
-    <section class="download">
-      <el-row>
-        <el-col :span="24">
-          <div class="banner">
-            <img src="../../static/img/service/banner.png" class="img-responsive" alt="" />
-            <!--遮罩层-->
-            <div class="half-mask">
-              <div class="text">
-                <h3>服 务</h3>
-                <p class="animated fadeInRight">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam assumenda beatae consequatur consequuntur dolore, eos eum fugiat ipsum mollitia perspiciatis, ratione, tempore vero voluptate. Aperiam eaque facere incidunt nihil optio.</p>
-              </div>
-            </div>
-          </div>
-        </el-col>
-        <el-col :span="24">
-          <div class="exploit">
-            <p>开发实施服务</p>
-            <el-row>
-              <el-col :span="12" :offset="5">
-                <div style="margin-bottom: 30px">
-                  <span style=" font-size: 30px;border-bottom:1px solid #f00;margin-bottom: 5px">标准开发实施</span>
-                  <span style="width: 30px;height: 30px;background: #f00;color: #fff;font-size: 20px">免费</span>
-                </div>
-                <div>
-                  <div>
-                    <img src="../../static/img/service/secondary.png" class="img-responsive" alt="">
-                    <img src="../../static/img/service/train.png" class="img-responsive" alt="">
-                  </div>
-                  <div>
-                    <img src="../../static/img/service/deploy.png" class="img-responsive" alt="">
-                  </div>
-                </div>
-              </el-col>
-            </el-row>
-          </div>
-        </el-col>
-      </el-row>
-    </section>
+  <section class="download">
+    <el-row>
+      <el-col :span="24">
+        <div class="banner">
+          <img src="../../static/img/download/Dbanner-.png" class="img-responsive" alt=""/>
+        </div>
+        <div class="app">
+          <img src="../../static/img/download/official.png" class="img-responsive" alt="">
+          <el-row :gutter="20">
+            <el-col :span="14" class="ios">
+              <img src="../../static/img/download/ios.png" class="img-responsive" alt="">
+              <img src="../../static/img/download/android.png" class="img-responsive" alt="">
+            </el-col>
+            <el-col :span="10">
+              <img src="../../static/img/download/qr_code.png" class="img-responsive" alt="">
+            </el-col>
+          </el-row>
+        </div>
+      </el-col>
+      <el-col :span="24" class="map">
+        <img src="../../static/img/about/contact-bg.png" class="img-responsive" alt="">
+        <div class="contact">
+          <p>联系我们</p>
+          <el-row :gutter="20">
+            <el-col :span="10" :offset="4" class="home">
+              <el-row :gutter="10">
+                <el-col :span="4">
+                  <ul>
+                    <li v-for="(home,index) in homeList" :key="home.index"> {{ home.li }} </li>
+                  </ul>
+                </el-col>
+                <el-col :span="4">
+                  <ul >
+                    <li v-for="(product,index) in productList" :key="product.index"> {{ product.li }} </li>
+                  </ul>
+                </el-col>
+                <el-col :span="4">
+                  <ul>
+                    <li  v-for="(service,index) in serviceList" :key="service.index"> {{ service.li }} </li>
+                  </ul>
+                </el-col>
+                <el-col :span="4">
+                  <ul>
+                    <li  v-for="(about,index) in aboutList" :key="about.index"> {{ about.li }} </li>
+                  </ul>
+                </el-col>
+                <el-col :span="4">
+                  <ul>
+                    <li  v-for="(download,index) in downloadList" :key="download.index"> {{ download.li }} </li>
+                    <li>
+                      <img src="../../static/img/download/qr_code.png" alt="">
+                    </li>
+                  </ul>
+                </el-col>
+              </el-row>
+
+            </el-col>
+            <el-col :span="10" class="address">
+              <ul>
+                <li v-for="(address,index) in addressList" :key="address.index"> {{address.li }} </li>
+              </ul>
+            </el-col>
+          </el-row>
+        </div>
+      </el-col>
+    </el-row>
+  </section>
 </template>
 
 
 <style lang="less" scoped>
-.download {
+  .download {
     margin-top: 70px;
-}
+    .clear {
+      clear: both;
+    }
+    .img-responsive {
+      display: block;
+      height: auto;
+      max-width: 100%;
+    }
+    .banner {
+      position: relative;
+    }
+    .app {
+      position: absolute;
+      top: 19%;
+      left: 21%;
+      width: 25%;
+      img {
+        margin-bottom: 20px;
+      }
+    }
+    .map {
+      position: relative;
+    }
+    .contact {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      top: 10%;
+      left: 0;
+      overflow: hidden;
+      p {
+        font-size: 30px;
+        color: #fff;
+        text-align: center;
+        margin-bottom: 50px;
+      }
+      ul{
+        float: left;
+        li:first-child{
+          margin-bottom: 30px;
+        }
+        li{
+          color: #fff;
+          font-size: 17px;
+          margin-bottom: 10px;
+        }
+        li:focus {
+          color: #1d90e6;
+        }
+      }
+    }
+
+  }
 </style>
 
-
 <script>
-export default {
+  export default {
+    data() {
+      return {
+        homeList: [
+          {li: '首页'},
+          {li: '系统特色'},
+          {li: '周期化管控'},
+          {li: '联系我们'}
+        ],
+        productList: [
+          {li: '产品'},
+          {li: '全网搜索'},
+          {li: '流程定制'},
+          {li: '统计分析'},
+          {li: '投后管理'},
+          {li: '文档管理'},
+          {li: '协同办公'},
+          {li: '风险预警'}
 
-}
+        ],
+        serviceList: [
+          {li:'服务'},
+          {li: '首页'},
+          {li: '系统特色'},
+          {li: '周期化管控'},
+          {li: '联系我们'}
+        ],
+        aboutList:[
+          {li:'关于'},
+          {li: '系统特色'},
+          {li: '周期化管控'},
+          {li: '联系我们'}
+        ],
+        downloadList:[
+          {li:'下载'}
+        ],
+        addressList:[
+          {li:'公司：陕西深度网路有限公司'},
+          {li:'地址：陕西省西安市高新区高新二路、12号协同大厦1层G1-10'},
+          {li:'热线：029-86687710'},
+          {li:'官方网址：www.sdcto.cn'},
+          {li:'工作时间：09:00-18:00(周一至周五)'}
+        ]
+      }
+    }
+  }
 </script>
--- src/views/service.vue
+++ src/views/service.vue
@@ -3,7 +3,7 @@
     <el-row>
       <el-col :span="24">
         <div class="banner">
-          <img src="../../static/img/service/banner.png" class="img-responsive" alt="" />
+          <img src="../../static/img/service/banner.png" class="img-responsive" alt=""/>
           <!--遮罩层-->
           <div class="half-mask">
             <div class="text">
@@ -18,22 +18,77 @@
         <div class="exploit">
           <p>开发实施服务</p>
           <el-row>
-            <el-col :span="12" :offset="5">
+            <el-col :span="17" push="4" pull="4">
               <div style="margin-bottom: 30px">
                 <span style=" font-size: 30px;border-bottom:1px solid #f00;margin-bottom: 5px">标准开发实施</span>
                 <span style="width: 30px;height: 30px;background: #f00;color: #fff;font-size: 20px">免费</span>
               </div>
-              <div>
-                <div>
-                  <img src="../../static/img/service/secondary.png" class="img-responsive" alt="">
-                  <img src="../../static/img/service/train.png" class="img-responsive" alt="">
-                </div>
-                <div>
+              <el-row style="overflow: hidden">
+                <el-col :span="16" style="flex-direction: column;display:flex">
+                  <div></div>
+                  <img src="../../static/img/service/secondary.png" class="img-responsive"
+                       style="flex-direction: column" alt="">
+                  <div class="shade"></div>
+                  <img src="../../static/img/service/train.png" class="img-responsive" style="flex-direction: column"
+                       alt="">
+                  <div class=""></div>
+                </el-col>
+                <el-col :span="8">
                   <img src="../../static/img/service/deploy.png" class="img-responsive" alt="">
-                </div>
-              </div>
+                  <div class=""></div>
+                </el-col>
+              </el-row>
             </el-col>
           </el-row>
+        </div>
+      </el-col>
+      <el-col :span="24">
+        <div class="customization">
+          <el-row>
+            <el-col :span="20" :offset="4">
+              <div style="margin-bottom: 40px">
+                <span style=" font-size: 30px;border-bottom:1px solid #f00;margin-bottom: 5px">定制开发实施</span>
+                <span style="width: 30px;height: 30px;background: #f00;color: #fff;font-size: 20px">可选</span>
+              </div>
+              <el-row>
+                <el-col :span="12" class="custom-made">
+                  <img src="../../static/img/service/customization1.png" class="img-responsive" alt="">
+                  <div class="custom-text">
+                    <p class="animated fadeInRight">
+                      本服务针对特定需求较多的客户按照实际研发工作量收费。
+</p>
+                  </div>
+                </el-col>
+                <el-col :span="12" :push="3" class="custom-made">
+                  <img src="../../static/img/service/customization2.png" class="img-responsive" alt="">
+                  <div class="custom-text">
+                    <p>服务包含标准系统的所有服务并根据用户功能需求进行深度定制开发。</p>
+                  </div>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+        </div>
+      </el-col>
+      <el-col :span="24">
+        <div class="after">
+          <img src="../../static/img/service/after_sale.png" class="img-responsive" alt=""/>
+          <div class="serve">
+            <h1>售后支持服务</h1>
+            <p>   领投者拥有一支技术精湛的专业运维服务队伍，具备先进的技术服务水平及丰富的软件系统运维经验。配合服务热线、远程技术支持和先进的服务管理机制，实现股权投资管理系统巡检、故障修复、数据库平台的安装、配置系统升级、系统更新及系统优化、系统高可靠性维护、数据存储及备份、技术培训等，有效的地帮助客户实现股权投资管理系统的平稳运行。并且由产品开发人员直接提供帮助。我们努力做到第一时间响应，保证所有疑问或请求会在一个工作日内回复</p>
+          </div>
+          <!--遮罩层-->
+          <div class="half-mask">
+            <div class="warranty">
+              <h3>质保售后服务（免费）</h3>
+              <p>
+                领头者所售标准系统及定制功能均提供6个月免费质保服务、
+                提供7*12电话技术支持服务、7*12远程技术支持服务
+              </p>
+              <h3>保后运维服务（可选）</h3>
+              <p>超过免费质保期后的产品支持服务，按年收费，年费率=系统总价*12%。提供7*12电话技术支持服务、7*12远程技术支持服务。</p>
+            </div>
+          </div>
         </div>
       </el-col>
     </el-row>
@@ -81,25 +136,129 @@
       color: #fff;
       padding-left: 80%;
       padding-top: 136px;
-      >h3 {
+      > h3 {
         padding-bottom: 5px;
         border-bottom: 5px solid #f00;
         margin-bottom: 50px;
       }
     }
     .exploit {
-      >p {
+      margin-bottom: 90px;
+      overflow: hidden;
+      .shade {
+        /*背景颜色（透明），高度，宽度，绝对定位，初始位置，过渡*/
+        z-index: 100;
+        background-color: rgba(0, 0, 0, 0.5);
+        height: 50%;
+        width: 67%;
+        position: absolute;
+        top: -40%;
+        left: 0;
+        transition: top 200ms;
+      }
+      .shade:hover {
+        top: 0;
+      }
+      > p {
         font-size: 30px;
         color: @font-color-title;
         text-align: center;
         margin-bottom: 90px;
+        transition: all 0.15s linear;
       }
-      >.row>p {
+      p:hover {
+        padding-left: 5%;
+      }
+      > .row > p {
         font-size: 30px;
         color: @font-color-title;
         text-align: left;
         margin-bottom: 30px;
       }
+    }
+    .customization {
+      margin-bottom: 90px;
+      img {
+        transition: all 0.3s linear;
+      }
+      img:hover {
+        transform: scale(1.1);
+      }
+      .custom-made {
+        width: 524px;
+        height: 312px;
+        position: relative;
+        overflow: hidden;
+      }
+      .custom-text {
+        width: 262px;
+        height:156px;
+        position: absolute;
+        top: 25%;
+        left: 25%;
+        color: #fff;
+        font-size: 20px;
+        padding-top: 10%;
+        p {
+          transition: all 0.15s linear;
+        }
+        p:hover {
+          padding-right: 5%;
+        }
+      }
+    }
+    .after {
+      height: 600px;
+      overflow: hidden;
+      position: relative;
+      .serve {
+        position: absolute;
+        left: 25%;
+        top: 20%;
+        text-align: right;
+        width: 400px;
+        h1 {
+          transition: all 0.15s linear;
+        }
+        h1:hover {
+          padding-right: 5%;
+        }
+        p {
+          transition: all 0.15s linear;
+        }
+        p:hover {
+          padding-right: 5%;
+        }
+      }
+      .half-mask {
+        /*背景颜色（透明），高度，宽度，绝对定位，初始位置，过渡*/
+        background-color: rgba(0, 0, 0, 0.6);
+        height: 100%;
+        width: 100%;
+        position: absolute;
+        top: 0;
+        left: 50%;
+        transition: left 200ms;
+        padding-right: 48px;
+        text-align: left;
+      }
+      div.half-mask:hover {
+        text-align: left;
+        left: 0;
+      }
+      .warranty {
+        padding-right: 80%;
+        padding-top: 136px;
+        color:#fff;
+        padding-left: 46px;
+        > h3 {
+          margin-bottom: 42px;
+        }
+        >p {
+          margin-bottom: 49px;
+        }
+      }
+
     }
   }
 </style>
